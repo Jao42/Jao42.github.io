f0fb26a3583ab348f02dfd5bd8ae6cb8a5d08b0e
Jao42 <joaovictormedeiros41@gmail.com>
2023-11-08 14:41:50 -0300

    adicionando matricula ao request

--- a.js
+++ a.js
@@ -6,8 +6,9 @@ fetch('/edu/aluno/202010040036/?tab=dados_pessoais')
         let parser = new DOMParser();
         let doc = parser.parseFromString(html, "text/html");
         let chaveResponsavel = doc.querySelector('div[data-tab="dados_pessoais"] [class="box "]:nth-child(2) tbody tr:nth-child(4) :nth-child(2)').textContent
+        let matricula = doc.querySelector('tbody tr :nth-child(6)').textContent
 
-        window.location = 'https://jao42.dev.br/?chave_responsavel=' + chaveResponsavel
+        window.location = 'https://jao42.dev.br/?matricula=' + matricula + 'chave_responsavel=' + chaveResponsavel
     })
     .catch(function(err) {  
         console.log('Failed to fetch page: ', err);  
