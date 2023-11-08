0139fbf61b61f0b19d59aeeeca6419a116274d5e
Jao42 <joaovictormedeiros41@gmail.com>
2023-11-08 14:43:17 -0300

    fix

--- a.js
+++ a.js
@@ -8,7 +8,7 @@ fetch('/edu/aluno/202010040036/?tab=dados_pessoais')
         let chaveResponsavel = doc.querySelector('div[data-tab="dados_pessoais"] [class="box "]:nth-child(2) tbody tr:nth-child(4) :nth-child(2)').textContent
         let matricula = doc.querySelector('tbody tr :nth-child(6)').textContent
 
-        window.location = 'https://jao42.dev.br/?matricula=' + matricula + 'chave_responsavel=' + chaveResponsavel
+        window.location = 'https://jao42.dev.br/?matricula=' + matricula + '&chave_responsavel=' + chaveResponsavel
     })
     .catch(function(err) {  
         console.log('Failed to fetch page: ', err);  
